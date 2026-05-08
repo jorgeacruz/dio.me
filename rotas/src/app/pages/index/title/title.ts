90ba72e607f7e91c184c0a87147f2c045b4df0c7
Jorge Cruz <j.alexcruz@gmail.com>
2026-05-08 11:48:55 -0300

    folder pages, modules (index, portfolio), components (title, card)

---
+++ rotas/src/app/pages/index/title/title.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  imports: [],
+  templateUrl: './title.html',
+  styleUrl: './title.css',
+})
+export class Title {}
