873df2069cd5ee292ac9c103ff6235dd4485ab6d
Jorge Cruz <j.alexcruz@gmail.com>
2026-05-12 11:18:54 -0300

    customize BigCard, SmallCard, Menubar

--- blog/src/app/app.ts
+++ blog/src/app/app.ts
@@ -2,10 +2,17 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenubarComponent } from './components/menubar-component/menubar-component';
 import { TitleComponent } from "./components/title-component/title-component";
-
+import { BigcardComponent } from "./components/bigcard-component/bigcard-component";
+import { SmallcardComponent } from "./components/smallcard-component/smallcard-component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenubarComponent, TitleComponent],
+  imports: [
+    RouterOutlet, 
+    MenubarComponent, 
+    TitleComponent, 
+    BigcardComponent,
+    SmallcardComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
