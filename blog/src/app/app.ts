9ef9fd9044fdc6261606fc3461dde690ea920f80
Jorge Cruz <j.alexcruz@gmail.com>
2026-05-12 10:40:22 -0300

    customize Title component

--- blog/src/app/app.ts
+++ blog/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenubarComponent } from './components/menubar-component/menubar-component';
+import { TitleComponent } from "./components/title-component/title-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenubarComponent],
+  imports: [RouterOutlet, MenubarComponent, TitleComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
