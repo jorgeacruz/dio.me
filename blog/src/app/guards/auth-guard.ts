463dac57422fa7f0f088a78761894e5c967182f7
jorgeacruz <j.alexcruz@gmail.com>
2026-05-27 18:08:41 -0300

    set guard to protect routes

--- blog/src/app/guards/auth-guard.ts
+++ blog/src/app/guards/auth-guard.ts
@@ -1,5 +1,6 @@
 import { CanActivateFn } from '@angular/router';
 
+// Lógica de autenticação aqui, retorne true se o usuário estiver autenticado, caso contrário, retorne false
 export const authGuard: CanActivateFn = (route, state) => {
-  return false;
+  return false; 
 };
