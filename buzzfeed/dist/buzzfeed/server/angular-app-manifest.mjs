7a888beed0524cdde9c6d55a357c5bb311380058
jorgeacruz <j.alexcruz@gmail.com>
2026-05-28 22:34:50 -0300

    build v.1.0.4

--- buzzfeed/dist/buzzfeed/server/angular-app-manifest.mjs
+++ buzzfeed/dist/buzzfeed/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 3976, hash: 'e73b21174ca09780c49ffa77490b6cd0a0c3ca931adb8bcc943e37dfe10eb723', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 954, hash: 'e0302210feeed6e1eaf4cb2d03d4ebfbcdc296a9bfa288f087c3e8bfe0e7dbbb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 10671, hash: 'bec7af485c4ef0247c7655820a9c6e51924053b8c308ca30182c838b71a70178', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-RYO37PTR.css': {size: 10014, hash: 'rnuQkJyZfxM', text: () => import('./assets-chunks/styles-RYO37PTR_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 3976, hash: '698d3d0b5c01a2cd2b83ad2327620d4348b2d9193dc0c30250a5301e4da46e7a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 954, hash: '55e7d720537994f7cd91f53602add127b0c4232a8598303d4ee39530e6d76d1e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 12286, hash: '43c438bcada44ee8b439ec7484c6c51c6adcbaf66d3b08ad479221b4845d04d2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-FP5GJ4ZR.css': {size: 10220, hash: 'mzGBfkch4aI', text: () => import('./assets-chunks/styles-FP5GJ4ZR_css.mjs').then(m => m.default)}
   },
 };
