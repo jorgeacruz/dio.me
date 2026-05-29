4e8f6c63e1091dd87e5bd17a74e6141a3abcd75e
jorgeacruz <j.alexcruz@gmail.com>
2026-05-28 21:01:05 -0300

    update quizz, spnner

--- buzzfeed/src/app/components/quizz/quizz.ts
+++ buzzfeed/src/app/components/quizz/quizz.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import QuizzService  from '../../data/quizz_questions.json';
 @Component({
   selector: 'app-quizz',
   imports: [],
   templateUrl: './quizz.html',
   styleUrl: './quizz.css',
 })
-export class Quizz {}
+export class Quizz implements OnInit{
+
+  title: string = "Escolha: Você é um Jedi ou um Sith?";
+  subtitle: string = "Responda as perguntas e descubra de qual lado da força você esta.";
+  //seleciona as perguntas
+  questions: any;
+  questionsSelected: any;
+  // armazena as respostas
+  answers: string[] = [];
+  answersSelected: string = '';
+  // armazena o resultado
+  questionsIndex: number = 0;
+  questionsMaxIndex: number = 0;
+  // finaaliza o quizz
+  finished: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(QuizzService){
+      this.finished = false;
+      this.title = QuizzService.title;
+     /* this.questions = QuizzService;
+      this.questionsSelected = this.questions[this.questionsIndex];
+      this.questionsMaxIndex = this.questions.length; */
+    }
+  }
+}
