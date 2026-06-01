71e5a90ff3724a8a0a0146c4c73a50d067cbd3d4
Jorge Cruz <j.alexcruz@gmail.com>
2026-06-01 11:24:44 -0300

    add styles.tsx

--- bat-generator/App.tsx
+++ bat-generator/App.tsx
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, Switch } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      
+      <Image source={require('./images/batsinal.jpg')} style={{width:250, height:250}} />
+      <Text style={styles.title}>Gerador de senha</Text>
+      <TextInput style={styles.Campo} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+     container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#211f1f',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  title:{
+    color:"#fff",
+    fontSize:26,
+    margin:12
+  },
+  Campo:{
+    borderWidth:1,
+    borderColor:"#fff",
+    borderRadius:4,
+    padding:2,
+    width:'80%',
+    height:40
+
+  }
+})
+
